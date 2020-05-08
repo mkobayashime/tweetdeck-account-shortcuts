eba4ca2077ef838e51c754ff28b65677ec8e6cfb
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-08 23:02:27 +0900

    Remove dead codes

--- src/background.js
+++ src/background.js
@@ -1,10 +1,8 @@
 const matchUrl = "tweetdeck.twitter.com"
 
-// eslint-disable-next-line no-undef
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete") {
     if (tab.url.indexOf(matchUrl) !== -1) {
-      // eslint-disable-next-line no-undef
       chrome.tabs.sendMessage(tabId, {
         type: "updated",
       })
