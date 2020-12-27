42d3890083b7ba4267c968a623fdd055ad327fa7
Masaki Kobayashi <contact@mkobayashi.me>
2020-12-27 16:47:30 +0900

    [Change] Use event.code instead of deprecated event.which

--- src/contentScript.js
+++ src/contentScript.js
@@ -49,39 +49,10 @@ const setShortcuts = (showBtn, application) => {
   }
 
   document.onkeyup = e => {
-    if (!isTyping()) {
-      switch (e.shiftKey && e.which) {
-        case 49:
-          clickAccount(0)
-          break
-        case 50:
-          clickAccount(1)
-          break
-        case 51:
-          clickAccount(2)
-          break
-        case 52:
-          clickAccount(3)
-          break
-        case 53:
-          clickAccount(4)
-          break
-        case 54:
-          clickAccount(5)
-          break
-        case 55:
-          clickAccount(6)
-          break
-        case 56:
-          clickAccount(7)
-          break
-        case 57:
-          clickAccount(8)
-          break
-        case 48:
-          clickAccount(9)
-          break
-      }
+    if (!isTyping() && e.shiftKey && e.code.includes("Digit")) {
+      const numKeyIndex = e.code.slice(-1)
+
+      numKeyIndex > 0 ? clickAccount(numKeyIndex - 1) : clickAccount(9)
     }
   }
 }
