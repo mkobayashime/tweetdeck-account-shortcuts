002d92ec45028591d9f273b5e88736e1f8c1dd63
Masaki Kobayashi <contact@mkobayashi.me>
2021-11-03 01:29:32 +0900

    Run Prettier

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict"
 
-const merge = require('webpack-merge');
+const merge = require("webpack-merge")
 
-const common = require('./webpack.common.js');
-const PATHS = require('./paths');
+const common = require("./webpack.common.js")
+const PATHS = require("./paths")
 
 // Merge webpack configuration files
 const config = merge(common, {
   entry: {
-    contentScript: PATHS.src + '/contentScript.js',
+    contentScript: PATHS.src + "/contentScript.js",
   },
-});
+})
 
-module.exports = config;
+module.exports = config
